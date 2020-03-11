c73fd653a67959279f1be4f2fc771211a40c13bf
M. Gage Morgan <gage@gages.blog>
2020-03-10 21:00:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(int)
+
+    remainder = int % 2
+
+    if int < 2 || (remainder == 0 && int != 2)
+        return false
+    else
+        (2..(int - 1)).each do |n|
+            if int % n == 0 
+                return false
+            end  
+        end
+    return true
+  end
+end
